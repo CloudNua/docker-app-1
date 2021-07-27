e9c68e1026099ecabe36878eb08739e4c7f6c08f
Enda Kelly <kelly.enda@gmail.com>
2021-07-27 10:59:23 +0100

    Added: docker updates 1. Main dockerfile for flask api 2. docker compose for api and other services 3. postgres dockerfile for db service

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import os  # new
+
 class BaseConfig:
     TESTING = False
 
@@ -12,3 +14,21 @@ class TestingConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     pass
+
+
+class BaseConfig:
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
+
+
+class DevelopmentConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
+
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
